dcc917f563280539961e65ed94834b3c472f324b
Akira Shinohara <k017c1067@it-neec.jp>
2018-10-31 14:57:09 +0900

    [new] ボイスチャット参加の通知を実装

--- main.js
+++ main.js
@@ -9,4 +9,17 @@ bot.on('ready', () => {
   console.log('Ready!');
 });
 
+bot.on('voiceChannelJoin', (member, newChannel) => {
+  const textChannel = newChannel.guild.channels.find((channel) => channel.type === 0);
+  const msg = `${member.username} が通話をはじめました`;
+  bot.createMessage(textChannel.id, msg);
+});
+
+bot.on("voiceChannelLeave", (member, oldChannel) => {
+  const textChannel = oldChannel.guild.channels.find((channel) => channel.type === 0);
+  const msg = `${member.username} が通話をやめました`;
+  bot.createMessage(textChannel.id, msg);
+});
+
 bot.connect();
+
