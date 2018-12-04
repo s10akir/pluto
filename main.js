0236125f7ecb30127d87aca1ceceda0c55fe2d04
Akira Shinohara <k017c1067@it-neec.jp>
2018-12-04 21:12:06 +0900

    [clean]

--- main.js
+++ main.js
@@ -3,29 +3,29 @@
 require('dotenv').config();
 const Eris = require('eris');
 const bot = new Eris.CommandClient(process.env.BOT_TOKEN, {}, {
-  prefix: '/'
+    prefix: '/'
 });
 
 
 bot.on('ready', () => {
-  console.log('Ready!');
+    console.log('Ready!');
 });
 
 bot.on('voiceChannelJoin', (member, newChannel) => {
-  const textChannel = newChannel.guild.channels.find((channel) => channel.type === 0);
-  const msg = `[bot] ${member.username} が通話をはじめました`;
-  bot.createMessage(textChannel.id, msg);
+    const textChannel = newChannel.guild.channels.find((channel) => channel.type === 0);
+    const msg = `[bot] ${member.username} が通話をはじめました`;
+    bot.createMessage(textChannel.id, msg);
 });
 
 bot.on("voiceChannelLeave", (member, oldChannel) => {
-  const textChannel = oldChannel.guild.channels.find((channel) => channel.type === 0);
-  const msg = `[bot] ${member.username} が通話をやめました`;
-  bot.createMessage(textChannel.id, msg);
+    const textChannel = oldChannel.guild.channels.find((channel) => channel.type === 0);
+    const msg = `[bot] ${member.username} が通話をやめました`;
+    bot.createMessage(textChannel.id, msg);
 });
 
 bot.registerCommand('ksv-sai', (message, args) => {
     if (args.length === 0) {
-      return '[!] please see help. run /help ksv-sai'
+        return '[!] please see help. run /help ksv-sai'
     }
 
     const command = args[0];
@@ -39,14 +39,14 @@ bot.registerCommand('ksv-sai', (message, args) => {
         default:
             return `[!] ${command}: command not found.`
     }
-  }, {
-    description: 'ksv-saiのあれこれ',
-    fullDescription: 'ksv-saiのpasswordを管理するコマンド。',
-    usage: ['```',
-            '/ksv-sai get          : 最新のpasswordを返す',
-            '/ksv-sai set PASSWORD : 新しいpasswordを登録する',
-            '```'
-           ].join('\n')
-  });
+    }, {
+        description: 'ksv-saiのあれこれ',
+        fullDescription: 'ksv-saiのpasswordを管理するコマンド。',
+        usage: ['```',
+                '/ksv-sai get          : 最新のpasswordを返す',
+                '/ksv-sai set PASSWORD : 新しいpasswordを登録する',
+                '```'
+               ].join('\n')
+    });
 
 bot.connect();
