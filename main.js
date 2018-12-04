4ca4e5452f73fd96e908ab4909561e5290fe12d8
Akira Shinohara <k017c1067@it-neec.jp>
2018-12-04 20:44:09 +0900

    [clean]

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 require('dotenv').config();
 const Eris = require('eris');
 const bot = new Eris.CommandClient(process.env.BOT_TOKEN, {}, {
-    prefix: '!' 
+  prefix: '!'
 });
 
 
@@ -24,4 +24,3 @@ bot.on("voiceChannelLeave", (member, oldChannel) => {
 });
 
 bot.connect();
-
