e07d48654bf071090ab7ea19f80814bbffb31672
Akira Shinohara <k017c1067@it-neec,jp>
2018-12-06 06:25:33 +0000

    [add] add bot description

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ require('date-utils');
 require('dotenv').config();
 const Eris = require('eris');
 const bot = new Eris.CommandClient(process.env.BOT_TOKEN, {}, {
-    prefix: '/'
+    prefix: '/',
+    owner: 'owner',
+    description: 'もじゃーん!'
 });
 
 // db
